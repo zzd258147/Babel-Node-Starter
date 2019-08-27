fe275756f77ef551aaafa60bc901facf50881b17
zzd258147 <3059450+zzd258147@users.noreply.github.com>
2019-08-27 16:16:45 +0800

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['eslint-config-alloy'],
+  extends: ['eslint-config-alloy', 'plugin:prettier/recommended'],
   globals: {
     // 这里填入你的项目需要的全局变量
     // 这里值为 false 表示这个全局变量不允许被重新赋值，比如：
